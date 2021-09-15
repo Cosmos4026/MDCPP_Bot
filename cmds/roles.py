d3442d376121c350d2b924defa9388610428209d
Cosmos4026 <88771492+Cosmos4026@users.noreply.github.com>
2021-09-15 22:42:42 +0800

    Update roles.py

--- cmds/roles.py
+++ cmds/roles.py
@@ -8,7 +8,7 @@ with open('setting.json', 'r', encoding='utf8') as jfile:
 
 
 class Roles(Cog_Extension):
-    """@commands.Cog.listener()
+    @commands.Cog.listener()
     async def on_member_join(self, member):
         channel = self.bot.get_channel(int(jdata['join_ch']))
         await channel.send(f'{member} 偷偷溜進來了')
@@ -17,7 +17,7 @@ class Roles(Cog_Extension):
     @commands.Cog.listener()
     async def on_member_remove(self, member):
         channel = self.bot.get_channel(int(jdata['leave_ch']))
-        await channel.send(f'{member} 打開門走出去了')"""
+        await channel.send(f'{member} 打開門走出去了')
         
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
@@ -26,52 +26,52 @@ class Roles(Cog_Extension):
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, data):
         
-        if data.channel_id == 867418185590505503 or data.channel_id == 867413719217602560:
+        if data.channel_id == int(jdata['role_ch']):
             if str(data.emoji) == '<:cat_A:885532118300639284>':
                 guild = self.bot.get_guild(data.guild_id)
-                role = guild.get_role(885534479039479860)
+                role = guild.get_role(int(jdata['roleA_ID']))
                 await data.member.add_roles(role)
 
             if str(data.emoji) == '<:cat_B:885532165419442247>':
                 guild = self.bot.get_guild(data.guild_id)
-                role = guild.get_role(885534277415104544)
+                role = guild.get_role(int(jdata['roleB_ID']))
                 await data.member.add_roles(role)
 
             if str(data.emoji) == '<:cat_C:885532297779085372>':
                 guild = self.bot.get_guild(data.guild_id)
-                role = guild.get_role(885534552787927153)
+                role = guild.get_role(int(jdata['roleC_ID']))
                 await data.member.add_roles(role)
 
             if str(data.emoji) == '<:doge:885532360324558868>':
                 guild = self.bot.get_guild(data.guild_id)
-                role = guild.get_role(885534667971899413)
+                role = guild.get_role(int(jdata['roleD_ID']))
                 await data.member.add_roles(role)
 
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, data):
-        if data.channel_id == 867418185590505503 or data.channel_id == 867413719217602560:
+        if data.channel_id == int(jdata['role_ch']):
             if str(data.emoji) == '<:cat_A:885532118300639284>':
                 guild = self.bot.get_guild(data.guild_id)
                 user = guild.get_member(data.user_id)
-                role = guild.get_role(885534479039479860)
+                role = guild.get_role(int(jdata['roleA_ID']))
                 await user.remove_roles(role)
 
             if str(data.emoji) == '<:cat_B:885532165419442247>':
                 guild = self.bot.get_guild(data.guild_id)
                 user = guild.get_member(data.user_id)
-                role = guild.get_role(885534277415104544)
+                role = guild.get_role(int(jdata['roleB_ID']))
                 await user.remove_roles(role)
             
             if str(data.emoji) == '<:cat_C:885532297779085372>':
                 guild = self.bot.get_guild(data.guild_id)
                 user = guild.get_member(data.user_id)
-                role = guild.get_role(885534552787927153)
+                role = guild.get_role(int(jdata['roleC_ID']))
                 await user.remove_roles(role)
 
             if str(data.emoji) == '<:doge:885532360324558868>':
                 guild = self.bot.get_guild(data.guild_id)
                 user = guild.get_member(data.user_id)
-                role = guild.get_role(885534667971899413)
+                role = guild.get_role(int(jdata['roleD_ID']))
                 await user.remove_roles(role)
 
 
